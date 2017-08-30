ab9c04926a589d6f818e528c30a97d2ac6228e67
javierlimonta <jlimonta@nauta.cu>
2017-08-30 16:15:24 +0200

    Toastr

--- src/app/components/angular2/angular2.module.ts
+++ src/app/components/angular2/angular2.module.ts
@@ -5,6 +5,7 @@ import {FormsModule} from '@angular/forms';
 import {SharedModule} from '../../global/shared.module';
 import {SelectModule} from 'ng2-select';
 import {ChartsModule} from 'ng2-charts';
+import {ToastyModule} from 'ng2-toasty';
 
 import {Angular2Component} from './angular2.component';
 import {SelectComponent} from './select/select.component';
@@ -16,14 +17,15 @@ import {ChartsComponent} from './charts/charts.component';
 import {LineComponent} from './charts/line/line.component';
 import {BarComponent} from './charts/bar/bar.component';
 import {PieComponent} from './charts/pie/pie.component';
+import {ToastyComponent} from './toasty/toasty.component';
 
 
 @NgModule({
   imports: [
-    CommonModule, ButtonsModule.forRoot(), FormsModule, SharedModule, SelectModule, ChartsModule
+    CommonModule, ButtonsModule.forRoot(), FormsModule, SharedModule, SelectModule, ChartsModule, ToastyModule.forRoot()
   ],
   declarations: [Angular2Component, SelectComponent, RichComponent, MultipleComponent, SimpleComponent, ChildrenComponent
-    , ChartsComponent, LineComponent, BarComponent, PieComponent],
+    , ChartsComponent, LineComponent, BarComponent, PieComponent, ToastyComponent],
   exports: [Angular2Component],
 })
 export class Angular2Module {
--- src/app/components/others/others.module.ts
+++ src/app/components/others/others.module.ts
@@ -1,17 +1,20 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 import {SharedModule} from '../../global/shared.module';
 import {NgProgressModule} from 'ngx-progressbar';
 import {ContextMenuModule} from 'ngx-contextmenu';
+import {ToastrModule} from 'ngx-toastr';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {ProgressComponent} from './progressbar/progress.component';
 import {OthersComponent} from './others.component';
 import {ContextMEComponent} from './contextmenu/contextmenu.component';
+import { ToastrComponent } from './toastr/toastr.component';
 
 @NgModule({
-  imports: [BrowserAnimationsModule, CommonModule, NgProgressModule, SharedModule, ContextMenuModule],
-  declarations: [OthersComponent, ProgressComponent, ContextMEComponent],
+  imports: [FormsModule,BrowserAnimationsModule, CommonModule, NgProgressModule, SharedModule, ContextMenuModule,ToastrModule.forRoot()],
+  declarations: [OthersComponent, ProgressComponent, ContextMEComponent, ToastrComponent],
   exports: [OthersComponent]
 })
 export class OthersModule {
